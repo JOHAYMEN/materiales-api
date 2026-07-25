754fcf6c5159ca7c7c2cd2fc6900ce6594d0089b
Johaymen <jjalvarez91@hotmail.com>
2026-07-24 19:51:09 -0500

    Agregar JavaDoc al enum EstadoMaterial

--- src/main/java/com/prueba/materialesapi/enums/EstadoMaterial.java
+++ src/main/java/com/prueba/materialesapi/enums/EstadoMaterial.java
@@ -1,5 +1,17 @@
 package com.prueba.materialesapi.enums;
-
+/**
+ * Enumeración que representa los posibles estados de un material dentro del sistema.
+ *
+ * <p>Los estados disponibles son:</p>
+ * <ul>
+ *   <li><b>ACTIVO:</b> El material se encuentra registrado y habilitado en el sistema.</li>
+ *   <li><b>DISPONIBLE:</b> El material está disponible para ser asignado.</li>
+ *   <li><b>ASIGNADO:</b> El material ya fue asignado y no se encuentra disponible.</li>
+ * </ul>
+ *
+ * @author Johaymen Alvarez Romero
+ * @since 1.0
+ */
 public enum EstadoMaterial {
     ACTIVO,
     DISPONIBLE,
